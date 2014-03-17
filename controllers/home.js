ed35b6c3b113def6c34a2f38c419d4e5d08f6e3e
Taylor Young <tdy721@gmail.com>
2014-03-16 20:40:46 -0500

    auto redirect users with servers to management page

--- controllers/home.js
+++ controllers/home.js
@@ -7,10 +7,18 @@ var api = new DigitalOceanAPI(secrets.digitalocean.client_id, secrets.digitaloce
  */
 
 exports.index = function(req, res) {
-  // if( req.user !== undefined ) {
-  //   res.redirect('/server/'+req.user.servers[0]);
-  // }
-  // else {
+  if( req !== undefined ) {
+    if( req.user !== undefined ) {
+      if( req.user.servers[0] !== undefined ) {
+        res.redirect('/server/'+req.user.servers[0]);
+      }else{
+        res.render('home',{title: 'Coming Soon' });   
+      }
+    }else{
+      res.render('home',{title: 'Coming Soon' });   
+    }
+  }
+  else {
     res.render('home',{title: 'Coming Soon' });
-  // }
+  }
 };
