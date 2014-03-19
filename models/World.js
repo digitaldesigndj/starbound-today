991453dd9dcc8a4dd999ca41a0942844cfd66990
Taylor Young <tdy721@gmail.com>
2014-03-19 00:01:50 -0500

    model updates

--- models/World.js
+++ models/World.js
@@ -4,8 +4,13 @@ var worldSchema = new mongoose.Schema({
   world_coords: { type: String, unique: true },
   email: { type: String, lowercase: true },
   server: Number,
+  ip_address: { type: String, default: '' },
+  nickname: { type: String, default: '' },
   name: { type: String, default: '' },
   description: { type: String, default: '' },
+  address: { type: String, default: '' },
+  owner: { type: String, default: '' },
+  system: { type: String, default: '' },
   saves: Array
 });
 
