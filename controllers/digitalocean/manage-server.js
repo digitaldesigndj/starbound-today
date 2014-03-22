e66db518549b2ca7eeb54455ab1040a8c344c50a
Taylor Young <tdy721@gmail.com>
2014-03-21 22:01:58 -0500

    better security for server management routes

--- controllers/digitalocean/manage-server.js
+++ controllers/digitalocean/manage-server.js
@@ -9,60 +9,53 @@ var User = require('../../models/User');
 exports.dropletPowerCycle = function(req, res) {
   User.findById(req.user.id, function (err, user) {
     if (err) return next(err);
-    user.starrypy = false; 
-    user.save( function (err) {
-      console.log( req.params.id );
-      api.dropletPowerCycle( req.params.id, function (err, event) {
-        if (err) return err;
-        req.flash('success', { msg: JSON.stringify(event) + " - your event is processing, refresh the page in 10 seconds" });
-        res.redirect('/server/'+req.params.id);
-        // res.redirect('/hosting/event?id='+event);
-      });
+    api.dropletPowerCycle( user.server, function (err, event) {
+      if (err) return err;
+      req.flash('success', { msg: JSON.stringify(event) + " - your event is processing, refresh the page in 10 seconds" });
+      res.redirect('/server/'+user.server);
+      // res.redirect('/hosting/event?id='+event);
     });
   });
 };
 
 // exports.dropletShutdown = function(req, res) {
-//   console.log( req.params.id );
-//   api.dropletShutdown( req.params.id, function (err, event) {
+//   console.log( user.server );
+//   api.dropletShutdown( user.server, function (err, event) {
 //     if (err) return err;
 //     req.flash('success', { msg: JSON.stringify(event) + " - your event is processing, refresh the page in 10 seconds" });
-//     res.redirect('/server/'+req.params.id);
+//     res.redirect('/server/'+user.server);
 //     // res.redirect('/hosting/event?id='+event);
 //   });
 // };
 
 exports.dropletPowerOff = function(req, res) {
-  console.log( req.user );
-  console.log( req.params.id );
   User.findById(req.user.id, function (err, user) {
     if (err) return next(err);
-    user.starrypy = false;
-    user.save( function (err) {
-      api.dropletPowerOff( req.params.id, function (err, event) {
-        if (err) return err;
-        req.flash('success', { msg: JSON.stringify(event) + " POWEROFF - Takes about 10 Seconds" });
-        res.redirect('/server/'+req.params.id);
-        // res.redirect('/hosting/event?id='+event);
-      });
+    api.dropletPowerOff( user.server, function (err, event) {
+      if (err) return err;
+      req.flash('success', { msg: JSON.stringify(event) + " POWEROFF - Takes about 10 Seconds" });
+      res.redirect('/server/'+user.server);
+      // res.redirect('/hosting/event?id='+event);
     });
   });
 };
 
 exports.dropletPowerOn = function(req, res) {
-  console.log( req.params.id );
-  api.dropletPowerOn( req.params.id, function (err, event) {
-    if (err) return err;
-    req.flash('success', { msg: JSON.stringify(event) + " POWERON - Takes about 20 Seconds, then another 30 for StarBound to start." });
-    res.redirect('/server/'+req.params.id);
-    // res.redirect('/hosting/event?id='+event);
+  User.findById(req.user.id, function (err, user) {
+    if (err) return next(err);
+    api.dropletPowerOn( user.server, function (err, event) {
+      if (err) return err;
+      req.flash('success', { msg: JSON.stringify(event) + " POWERON - Takes about 20 Seconds, then another 30 for StarBound to start." });
+      res.redirect('/server/'+user.id);
+      // res.redirect('/hosting/event?id='+event);
+    });
   });
 };
 
 exports.dropletSnapshot = function(req, res) {
   User.findById( req.user.id, function (err, user) {
     if (err) return next(err);
-    api.dropletSnapshot( req.params.id, { name: user.profile.domain }, function (err, event_id) {
+    api.dropletSnapshot( user.server, { name: user.profile.domain }, function (err, event_id) {
       if (err) return err;
       api.eventGet(event_id, function ( error, event ) {
         req.flash('success', { msg: JSON.stringify(event) + " - your event is processing, this usually takes about 10 min." });
@@ -76,20 +69,20 @@ exports.dropletSnapshot = function(req, res) {
 // exports.dropletRestore = function(req, res) {
 //   console.log( req.user.server.image );
 
-//   // api.dropletRestore( req.params.id, req.user.server.image, function (err, event) {
+//   // api.dropletRestore( user.server, req.user.server.image, function (err, event) {
 //   //   if (err) return err;
 //   //   req.flash('success', { msg: JSON.stringify(event) + " - your event is processing, refresh the page in 10 seconds" });
-//   //   res.redirect('/server/'+req.params.id);
+//   //   res.redirect('/server/'+user.server);
 //   //   // res.redirect('/hosting/event?id='+event);
 //   // });
 // };
 
 // exports.dropletRebuild = function(req, res) {
-//   console.log( req.params.id );
-//   api.dropletRebuild( req.params.id, function (err, event) {
+//   console.log( user.server );
+//   api.dropletRebuild( user.server, function (err, event) {
 //     if (err) return err;
 //     req.flash('success', { msg: JSON.stringify(event) + " - your event is processing, refresh the page in 10 seconds" });
-//     res.redirect('/server/'+req.params.id);
+//     res.redirect('/server/'+user.server);
 //     // res.redirect('/hosting/event?id='+event);
 //   });
 // };
@@ -97,7 +90,7 @@ exports.dropletSnapshot = function(req, res) {
 exports.dropletDestroy = function(req, res) {
   User.findById(req.user.id, function (err, user) {
     if (err) return err;
-    api.dropletGet( req.params.id, function (err, droplet) {
+    api.dropletGet( user.server, function (err, droplet) {
       if (err) return err;
       dropletUtils.dropletDestroy( user, droplet, function( event ) {
         req.flash('warning', { msg: "Server Destroyed" });
@@ -107,13 +100,13 @@ exports.dropletDestroy = function(req, res) {
   });
 };
 
-exports.selectImage = function (req, res) {
-  // size_id 66 = 512MB
-  api.imageGetAll(function (error, images) {
-    res.render('hosting/make/server', {
-      'title': 'Rebuild',
-      'tagline': 'Pick an Image to rebuild from',
-      'images': images
-    });
-  });
-};
+// exports.selectImage = function (req, res) {
+//   // size_id 66 = 512MB
+//   api.imageGetAll(function (error, images) {
+//     res.render('hosting/make/server', {
+//       'title': 'Rebuild',
+//       'tagline': 'Pick an Image to rebuild from',
+//       'images': images
+//     });
+//   });
+// };
