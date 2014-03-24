7a87c9fd7fcf712c999df748bae88b10f9166760
Taylor Young <tdy721@gmail.com>
2014-03-24 02:25:38 -0500

    var type fix, silly + concats…

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -16,19 +16,19 @@ module.exports = function( req, res ) {
             if (err) return next(err);
             var bonus = 0;
             if( user.special_redeemed != false ){
-              if( purchase.offer_code == 'boundstar') {
+              if( purchase.offer_code === 'boundstar') {
                 bonus = 2;
                 user.special_redeemed = true;
               }
             }
-            user.server_tokens = +user.server_tokens + ( +purchase.price / 50 ) + bonus;
+            user.server_tokens = parseInt(user.server_tokens) + ( parseInt(purchase.price) / 50 ) + parseInt(bonus);
             user.save(function (err) {
               if (err) return next(err);
               purchase.claimed = true;
               purchase.save(function(err) {
                 if (err) { return err; }
                 console.log( 'purchase claimed' );
-                req.flash('success', { msg: 'Redeemed '+( +purchase.price / 50 ) + bonus + ' tokens to account' + purchase.email + '. Thanks!'});
+                req.flash('success', { msg: 'Redeemed '+( parseInt(purchase.price) / 50 ) + parseInt(bonus) + ' tokens to account ' + purchase.email + '. Thanks!'});
                 res.redirect('/');
               });
             });
