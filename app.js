c8a04a964f61d9e67d46e3306f88f0dd85cab0dc
Taylor Young <tdy721@gmail.com>
2014-03-29 02:03:30 -0500

    terms and conditions

--- app.js
+++ app.js
@@ -120,6 +120,7 @@ app.get('/monthly', function(req, res){ res.redirect('/arcade') });
 app.get('/arcade', pricingController.getArcadePricing);
 
 app.get('/', homeController.index);
+app.get('/terms', homeController.terms);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
