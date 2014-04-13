8658f416ea36afb5da4c31775d4c950f23eecb5a
Taylor Young <tdy721@gmail.com>
2014-04-12 23:02:25 -0500

    gumroad add now handles donations

--- gumroad.js
+++ gumroad.js
@@ -97,45 +97,87 @@ app.post('/gumroad', function( req, res ) {
       offer_code: req.body.offer_code || false
     });
 
-    User.findOne({ email: req.body.email }, function(err, user) {
-      if (err) return next(err);
-      console.log(user);
-      if( user != null ) {
-        console.log(req.body.full_name);
-        // user.name = req.body.full_name;
-        var total = req.body.price;
-        if( user.special_redeemed != false ){
-          if( req.body.offer_code == 'boundstar') {
-            total = +req.body.price + 100;
-            user.special_redeemed = true;
+    if( req.body.permalink != 'boundstar' ) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (err) return next(err);
+        console.log(user);
+        if( user != null ) {
+          console.log(req.body.full_name);
+          // user.name = req.body.full_name;
+          var total = req.body.price;
+          if( user.special_redeemed != false ){
+            if( req.body.offer_code == 'boundstar') {
+              total = +req.body.price + 100;
+              user.special_redeemed = true;
+            }
           }
+          user.server_tokens = +user.server_tokens + ( +total / 50 );
+          purchase.claimed = true;
+          purchase.save(function(err) {
+            if (err) { return err; }
+              console.log( 'purchase saved' );
+              user.save(function(err) {
+                console.log( 'User bought server tokens, '+req.body.email+' has been creditied' );
+                smtpTransport.sendMail(mailOptions, function(err) {
+                  if (err) { return err; }
+                  res.redirect('/');
+                });
+              });
+          });
         }
-        user.server_tokens = +user.server_tokens + ( +total / 50 );
-        purchase.claimed = true;
-        purchase.save(function(err) {
-          if (err) { return err; }
-            console.log( 'purchase saved' );
-            user.save(function(err) {
-              console.log( 'User bought server tokens, '+req.body.email+' has been creditied' );
-              smtpTransport.sendMail(mailOptions, function(err) {
+        else{
+          console.log( 'eMailed purchase code to '+req.body.email+', Waiting to be claimed' );
+          purchase.save(function(err) {
+            if (err) { return err; }
+              console.log( 'purchase saved' );
+              smtpTransport.sendMail(mailOptions, function(err, response) {
                 if (err) { return err; }
+                console.log( response );
                 res.redirect('/');
               });
-            });
-        });
-      }else{
-        console.log( 'eMailed purchase code to '+req.body.email+', Waiting to be claimed' );
-        purchase.save(function(err) {
-          if (err) { return err; }
-            console.log( 'purchase saved' );
-            smtpTransport.sendMail(mailOptions, function(err, response) {
-              if (err) { return err; }
-              console.log( response );
-              res.redirect('/');
-            });
-        });
-      }
-    });
+          });
+        }
+      });
+    }
+    else {
+
+      mailOptions.subject: 'Thanks for your donation to Boundstar',
+      mailOptions.text: 'Your donation code is ' + hash
+      // This is a donation
+      Player.findOne({ email: req.body.email }, function(err, player) {
+        if (err) return next(err);
+        console.log(player);
+        if( player != null ) {
+          var total = req.body.price;
+          player.donations.push( req.body.order_number );
+          player.donation_amount = +player.donation_amount + +req.body.price;
+          purchase.save(function(err) {
+            if (err) { return err; }
+              console.log( 'purchase saved' );
+              player.save(function(err) {
+                console.log( 'Player donated, '+req.body.email+' has been creditied' );
+                smtpTransport.sendMail(mailOptions, function(err) {
+                  if (err) { return err; }
+                  res.redirect('/');
+                });
+              });
+          });
+        }
+        else{
+          console.log( 'eMailed member code to '+req.body.email+', Waiting to be claimed' );
+          purchase.save(function(err) {
+            if (err) { return err; }
+              console.log( 'purchase saved' );
+              mailOptions.text: 'Your donation code is ' + hash + '. We could not find an account with your email address, please contact a Boundstar admin with your code or forward this email to tdy721@gmail.com'
+              smtpTransport.sendMail(mailOptions, function(err, response) {
+                if (err) { return err; }
+                console.log( response );
+                res.redirect('/');
+              });
+          });
+        }
+      });
+    }
   }
   else {
     return res.send("go away, seller_id did not match.");
